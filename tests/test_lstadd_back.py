6258a1438653690fb6f962e668d27571e0c831a8
Axel BRISSE <axbrisse@e1r8p7.clusters.42paris.fr>
2022-12-06 15:16:59 +0100

    first lstadd_(back|front) call tested

---
+++ tests/test_lstadd_back.py
@@ -0,0 +1,21 @@
+from ctypes import byref, c_char_p, POINTER
+from test_lstnew import ft_lstnew
+from utils import libft, t_list
+
+ft_lstadd_back = libft.ft_lstadd_back
+ft_lstadd_back.argtypes = (
+    POINTER(POINTER(t_list)),
+    POINTER(t_list),
+)
+ft_lstadd_back.restype = None
+
+
+def test_lstadd_back():
+    lst = POINTER(t_list)()
+    ft_lstadd_back(byref(lst), ft_lstnew(69))
+    ft_lstadd_back(byref(lst), ft_lstnew(42))
+    ft_lstadd_back(byref(lst), ft_lstnew(b"wololo"))
+    assert lst.contents.content == 69
+    assert lst.contents.next.contents.content == 42
+    assert c_char_p(lst.contents.next.contents.next.contents.content).value == b"wololo"
+    assert not lst.contents.next.contents.next.contents.next
--- tests/test_lstadd_front.py
+++ tests/test_lstadd_front.py
@@ -1,4 +1,4 @@
-from ctypes import *  # TODO
+from ctypes import byref, c_char_p, POINTER
 from test_lstnew import ft_lstnew
 from utils import libft, t_list
 
@@ -11,32 +11,11 @@ ft_lstadd_front.restype = None
 
 
 def test_lstadd_front():
-    lst = ft_lstnew(69)
+    lst = POINTER(t_list)()
+    ft_lstadd_front(byref(lst), ft_lstnew(69))
     ft_lstadd_front(byref(lst), ft_lstnew(42))
     ft_lstadd_front(byref(lst), ft_lstnew(b"wololo"))
     assert c_char_p(lst.contents.content).value == b"wololo"
     assert lst.contents.next.contents.content == 42
     assert lst.contents.next.contents.next.contents.content == 69
     assert not lst.contents.next.contents.next.contents.next
-
-
-l1 = None
-l2 = ft_lstnew(42)
-for lst in (l1, l2):
-    print(lst)
-    try:
-        print(pointer(lst))
-    except Exception as e:
-        print(e)
-        pass
-    try:
-        print(byref(lst))
-    except Exception as e:
-        print(e)
-        pass
-    try:
-        print(hex(addressof(lst)))
-    except Exception as e:
-        print(e)
-        pass
-    print("-" * 40)
