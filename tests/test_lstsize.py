774699fa40082bfed7140602689081d09b01f022
Axel BRISSE <axbrisse@e1r8p7.clusters.42paris.fr>
2022-12-06 15:20:51 +0100

    test_lstsize

---
+++ tests/test_lstsize.py
@@ -0,0 +1,19 @@
+from ctypes import byref, c_int, POINTER
+from test_lstadd_front import ft_lstadd_front
+from test_lstnew import ft_lstnew
+from utils import libft, t_list
+
+ft_lstsize = libft.ft_lstsize
+ft_lstsize.argtypes = (POINTER(t_list),)
+ft_lstsize.restype = c_int
+
+
+def test_lstadd_front():
+    lst = POINTER(t_list)()
+    assert ft_lstsize(lst) == 0
+    ft_lstadd_front(byref(lst), ft_lstnew(69))
+    assert ft_lstsize(lst) == 1
+    ft_lstadd_front(byref(lst), ft_lstnew(42))
+    assert ft_lstsize(lst) == 2
+    ft_lstadd_front(byref(lst), ft_lstnew(b"wololo"))
+    assert ft_lstsize(lst) == 3
