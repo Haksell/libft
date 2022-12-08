1364225018a1b1454f55d6d6bbf4bce4b2bf80c8
Axel BRISSE <axbrisse@e3r12p23.clusters.42paris.fr>
2022-12-08 15:15:15 +0100

    test putchar

---
+++ tests/test_putchar_fd.py
@@ -0,0 +1,18 @@
+from ctypes import c_char, c_int
+from utils import libft
+import os
+
+ft_putchar_fd = libft.ft_putchar_fd
+ft_putchar_fd.argtypes = (c_char, c_int)
+ft_putchar_fd.restype = None
+
+FILENAME = "tests/files/ft_putchar_fd"
+
+
+def test_putchar_fd():
+    with open(FILENAME, "w") as fp:
+        fd = fp.fileno()
+        for c in "ft_putchar_fd":
+            ft_putchar_fd(ord(c), fd)
+    assert open(FILENAME).read() == "ft_putchar_fd"
+    os.remove(FILENAME)
