59a9bf2e49cc7f071572d8c92c686852524afdb9
Axel BRISSE <axbrisse@e1r11p22.clusters.42paris.fr>
2022-12-08 08:53:06 +0100

    test_strlen

---
+++ tests/test_strlen.py
@@ -0,0 +1,12 @@
+from ctypes import create_string_buffer, c_char_p, c_size_t
+from utils import libft
+
+ft_strlen = libft.ft_strlen
+ft_strlen.argtypes = (c_char_p,)
+ft_strlen.restype = c_size_t
+
+
+def test_strlen():
+    for b in (b"", b"a", b"ab", b"abc", b"abcd", b"abcde", b"ab\0cde"):
+        sb = create_string_buffer(b)
+        assert ft_strlen(sb) == libft.strlen(b)
