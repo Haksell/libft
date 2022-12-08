492183a33de8a674520f7e010c27af99d673dabe
Axel BRISSE <axbrisse@e1r6p5.clusters.42paris.fr>
2022-12-08 10:57:32 +0100

    improve strlcat

--- srcs/strings/ft_strlcat.c
+++ srcs/strings/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: axbrisse <axbrisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 14:59:03 by axbrisse          #+#    #+#             */
-/*   Updated: 2022/12/08 10:50:29 by axbrisse         ###   ########.fr       */
+/*   Updated: 2022/12/08 10:57:26 by axbrisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 
-	src_len = ft_strlen(src);
 	i = 0;
 	while (i < size && dst[i] != '\0')
 		i++;
