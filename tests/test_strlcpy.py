93d210e79a1b17a9827cc394c7b81c6ada7e278d
Axel BRISSE <axbrisse@e1r11p22.clusters.42paris.fr>
2022-12-08 09:24:36 +0100

    test_strlcpy

---
+++ tests/test_strlcpy.py
@@ -0,0 +1,32 @@
+from ctypes import c_char_p, c_size_t, create_string_buffer
+from utils import libft
+
+ft_strlcpy = libft.ft_strlcpy
+ft_strlcpy.argtypes = (c_char_p, c_char_p, c_size_t)
+ft_strlcpy.restype = c_size_t
+
+
+def compare(n):
+    s = create_string_buffer(b"abcdefghijklmnopqrstuvwxyz")
+    buffer = create_string_buffer(27)
+    ans = ft_strlcpy(buffer, s, n)
+    assert ans == 26
+    assert buffer.value == (b"" if n == 0 else s.value[: n - 1])
+
+
+def test_zero():
+    compare(0)
+
+
+def test_smaller():
+    for i in range(1, 26):
+        compare(i)
+
+
+def test_same():
+    compare(26)
+
+
+def test_bigger():
+    for i in range(27, 69):
+        compare(i)
