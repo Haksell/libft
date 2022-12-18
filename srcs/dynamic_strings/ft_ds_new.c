26657f69da6ea76fa6023cdb2ee7140b4c2e996d
Axel Brisse <axel.brisse@gmail.com>
2022-12-18 19:07:30 +0100

    cancel ft_ds_new accepts NULL

--- srcs/dynamic_strings/ft_ds_new.c
+++ srcs/dynamic_strings/ft_ds_new.c
@@ -6,7 +6,7 @@
 /*   By: axbrisse <axbrisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 10:00:38 by axbrisse          #+#    #+#             */
-/*   Updated: 2022/12/18 19:04:56 by axbrisse         ###   ########.fr       */
+/*   Updated: 2022/12/18 19:06:34 by axbrisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,7 @@ t_dynamic_string	ft_ds_new(char *s)
 	const size_t		length = ft_strlen(s);
 	t_dynamic_string	dynamic_string;
 
-	if (s == NULL)
-		dynamic_string.content = NULL;
-	else
-		dynamic_string.content = ft_substr(s, 0, length);
+	dynamic_string.content = ft_substr(s, 0, length);
 	dynamic_string.length = length;
 	dynamic_string.capacity = length + 1;
 	return (dynamic_string);
