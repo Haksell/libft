c46c3ae42cc5bab727b31b6448efcf3c52205e26
Axel Brisse <axel.brisse@gmail.com>
2022-12-21 05:25:58 +0100

    important modification

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: axbrisse <axbrisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 11:27:39 by axbrisse          #+#    #+#             */
-/*   Updated: 2022/12/21 05:24:16 by axbrisse         ###   ########.fr       */
+/*   Updated: 2022/12/21 05:25:43 by axbrisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 4096
 # endif
-
 # define FILE_DESCRIPTORS 256
+
 # define DECIMAL "0123456789"
 # define HEXA_LOWER "0123456789abcdef"
 # define HEXA_UPPER "0123456789ABCDEF"
