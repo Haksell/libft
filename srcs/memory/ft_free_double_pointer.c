16b72919059a67e1448fe319b4ce209c2b773d57
Axel Brisse <axel.brisse@gmail.com>
2023-02-05 03:56:56 +0100

    fix ft_free_double_pointer

--- srcs/memory/ft_free_double_pointer.c
+++ srcs/memory/ft_free_double_pointer.c
@@ -6,7 +6,7 @@
 /*   By: axbrisse <axbrisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 22:24:08 by axbrisse          #+#    #+#             */
-/*   Updated: 2023/01/16 11:54:32 by axbrisse         ###   ########.fr       */
+/*   Updated: 2023/02/05 03:56:44 by axbrisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	ft_free_double_pointer(void **grid, size_t height)
 	if (grid == NULL)
 		return ;
 	i = 0;
-	while (i < height)
+	while (i < height && grid[i] != NULL)
 	{
 		ft_free(grid + i);
 		++i;
