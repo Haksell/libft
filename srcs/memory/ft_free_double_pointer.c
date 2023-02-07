99301ff760f0777b6e5a27676787768b9f9cabab
Axel Brisse <axel.brisse@gmail.com>
2023-02-07 13:11:13 +0100

    ft_free(&(*grid)[i]);

--- srcs/memory/ft_free_double_pointer.c
+++ srcs/memory/ft_free_double_pointer.c
@@ -6,7 +6,7 @@
 /*   By: axbrisse <axbrisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 22:24:08 by axbrisse          #+#    #+#             */
-/*   Updated: 2023/02/07 12:50:10 by axbrisse         ###   ########.fr       */
+/*   Updated: 2023/02/07 13:11:07 by axbrisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	ft_free_double_pointer(void ***grid, size_t height)
 	i = 0;
 	while (i < height && (*grid)[i] != NULL)
 	{
-		ft_free(*grid + i);
+		ft_free(&(*grid)[i]);
 		++i;
 	}
 	ft_free((void **)grid);
